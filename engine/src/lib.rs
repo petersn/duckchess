25cd559b1f4d4ea96ac1cf89e050e09ced68dc2b
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2022-10-20 15:29:49 -0700

    More changes.

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+use serde::{Serialize, Deserialize, ser::SerializeSeq};
 
 #[wasm_bindgen]
 extern "C" {
@@ -6,8 +7,106 @@ extern "C" {
     fn log(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn rust_func() {
-    log("Hello from Rust");
+#[derive(Hash, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct CastlingRights {
+    king_side:  bool,
+    queen_side: bool,
 }
 
+#[derive(Hash)]
+struct BitBoard(u64);
+
+impl Serialize for BitBoard {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut bytes = serializer.serialize_seq(Some(8))?;
+        for i in 0..8 {
+            let byte = (self.0 >> (i * 8)) as u8;
+            bytes.serialize_element(&byte)?;
+        }
+        bytes.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for BitBoard {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let bytes = Vec::<u8>::deserialize(deserializer)?;
+        let mut board = 0;
+        for (i, byte) in bytes.iter().enumerate() {
+            board |= (*byte as u64) << (i * 8);
+        }
+        Ok(BitBoard(board))
+    }
+}
+
+
+#[derive(Hash, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct State {
+    pawns:           [BitBoard; 2],
+    knights:         [BitBoard; 2],
+    bishops:         [BitBoard; 2],
+    rooks:           [BitBoard; 2],
+    queens:          [BitBoard; 2],
+    kings:           [BitBoard; 2],
+    duck:            BitBoard,
+    en_passant:      BitBoard,
+    castling_rights: [CastlingRights; 2],
+    is_duck_move:    bool,
+}
+
+fn starting_state() -> State {
+    State {
+        pawns:           [BitBoard(0x000000000000ff00), BitBoard(0x00ff000000000000)],
+        knights:         [BitBoard(0x0000000000000042), BitBoard(0x4200000000000000)],
+        bishops:         [BitBoard(0x0000000000000024), BitBoard(0x2400000000000000)],
+        rooks:           [BitBoard(0x0000000000000081), BitBoard(0x8100000000000000)],
+        queens:          [BitBoard(0x0000000000000008), BitBoard(0x0800000000000000)],
+        kings:           [BitBoard(0x0000000000000010), BitBoard(0x1000000000000000)],
+        duck:            BitBoard(0),
+        en_passant:      BitBoard(0),
+        castling_rights: [
+            CastlingRights {
+                king_side:  true,
+                queen_side: true,
+            },
+            CastlingRights {
+                king_side:  true,
+                queen_side: true,
+            },
+        ],
+        is_duck_move:    false,
+    }
+}
+
+#[wasm_bindgen]
+pub struct Engine {
+    state: State,
+}
+
+#[wasm_bindgen]
+impl Engine {
+    pub fn get_state(&self) -> JsValue {
+        serde_wasm_bindgen::to_value(&self.state).unwrap_or_else(|e| {
+            log(&format!("Failed to serialize state: {}", e));
+            JsValue::NULL
+        })
+    }
+
+    pub fn set_state(&mut self, state: JsValue) {
+        self.state = serde_wasm_bindgen::from_value(state).expect("serialization");
+    }
+}
+
+#[wasm_bindgen]
+pub fn new_engine() -> Engine {
+    Engine {
+        state: starting_state(),
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import './App.css';
-import init, { rust_func } from 'engine';
+import init, { new_engine, Engine } from 'engine';
 
 function AppWithWasm() {
-  rust_func();
+  const [engine, setEngine] = React.useState<Engine | null>(null);
+  React.useEffect(() => {
+    setEngine(new_engine());
+  }, []);
+  if (engine === null)
+    return <div>Initializing engine...</div>;
+
+  const state = engine.get_state();
+
+  const board = [];
+  for (let y = 0; y < 8; y++)
+    board.push([null, null, null, null, null, null, null, null]);
+
+  for (let y = 0; y < 8; y++) {
+    for (let player = 0; player < 2; player++) {
+      for (const name of ['pawns', 'knights', 'bishops', 'rooks', 'queens', 'kings']) {
+        let byte = state[name][player][y];
+        for (let x = 0; x < 8; x++) {
+          const hasPiece = byte & 1;
+          byte = byte >> 1;
+          if (!hasPiece)
+            continue;
+        }
+      }
+    }
+  }
+
   return (
-    <div>Hello!</div>
+    <div>
+      {JSON.stringify(state)}
+    </div>
   );
 }
 
@@ -17,7 +45,7 @@ function App() {
       .then(() => setInitialized(true))
       .catch(console.error);
   }, []);
-  return initialized ? <AppWithWasm /> : <div>Loading...</div>;
+  return initialized ? <AppWithWasm /> : <div>Loading WASM...</div>;
 }
 
 export default App;
