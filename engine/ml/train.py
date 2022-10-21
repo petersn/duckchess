6e5980d3abf6600e631bea93139b48c3c202fd0e
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2022-10-21 13:09:46 -0700

    More stuff.

---
+++ engine/ml/train.py
@@ -0,0 +1,16 @@
+import json
+import glob
+import numpy as np
+import torch
+
+all_games = []
+for path in glob.glob("games/games-*.json"):
+    with open(path) as f:
+        for line in f:
+            all_games.append(json.loads(line))
+
+print("Total games:", len(all_games))
+print("Total moves:", sum(len(game["moves"]) for game in all_games))
+
+
+
