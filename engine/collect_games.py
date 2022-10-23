eadbc32a1bab070a6681f7bc9c99b14a3c70a1aa
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2022-10-23 12:29:19 -0700

    More data.

--- engine/collect_games.py
+++ engine/collect_games.py
@@ -9,3 +9,11 @@ for path in glob.glob("games/games-*.json"):
 
 print("Total games:", len(all_games))
 print("Total moves:", sum(len(game["moves"]) for game in all_games))
+
+import collections
+c = collections.Counter()
+for game in all_games:
+    c[str(game["moves"][0])] += 1
+for k, v in c.items():
+    print(k, "->", v)
+
