f475b311c86bf04b317746fbeea555ef72e30f93
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2022-11-14 16:58:56 -0800

    Oopsies.

--- engine/src/web.rs
+++ engine/src/web.rs
@@ -5,7 +5,7 @@ use wasm_bindgen::prelude::*;
 
 use crate::inference::{FEATURES_SIZE, POLICY_LEN};
 use crate::inference_web::MAX_BATCH_SIZE;
-use crate::mcts::PendingPath;
+use crate::mcts::{PendingPath, SearchParams};
 use crate::{inference, inference_web, mcts, rules::Move, search};
 
 const MAX_STEPS_BEFORE_INFERENCE: usize = 40 * MAX_BATCH_SIZE;
