bb69e7d9b811c69e977a81c0afe52ef9ebf4bc0b
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2022-11-16 12:02:25 -0800

    More work on training nnue

--- engine/ml/train_nnue.py
+++ engine/ml/train_nnue.py
@@ -53,28 +53,49 @@ class Nnue(torch.nn.Module):
             self.clipped_relu,
             torch.nn.Linear(32, 1),
         )
-        self.white_main = make_net()
-        self.black_main = make_net()
-        self.white_duck = make_net()
-        self.black_duck = make_net()
+        self.networks = torch.nn.ModuleList([make_net() for _ in range(4 * 4)])
+        #self.white_main = make_net()
+        #self.black_main = make_net()
+        #self.white_duck = make_net()
+        #self.black_duck = make_net()
 
     def adjust_leak(self, new_leak):
         self.clipped_relu.leak = new_leak
 
-    def forward(self, indices, offsets, which_model):
+    def forward(self, indices, offsets, which_model, lengths):
         accum = self.main_embed(indices, offsets) + self.main_bias
         embedding, psqt = accum[:, :-1], accum[:, -1:]
         embedding = self.clipped_relu(embedding)
-        white_main = self.white_main(embedding)
-        black_main = self.black_main(embedding)
-        white_duck = self.white_duck(embedding)
-        black_duck = self.black_duck(embedding)
-        data = torch.stack([black_main, white_main, black_duck, white_duck])
+        network_outputs = [net(embedding) for net in self.networks]
+        #white_main = self.white_main(embedding)
+        #black_main = self.black_main(embedding)
+        #white_duck = self.white_duck(embedding)
+        #black_duck = self.black_duck(embedding)
+        #data = torch.stack([black_main, white_main, black_duck, white_duck])
+        data = torch.stack(network_outputs)
+        game_phase = lengths // 17
+        which_model = which_model + game_phase * 4
         value = data[which_model, torch.arange(len(which_model))]
         return self.tanh(value + psqt)
 
-#model = Nnue()
-#print("Parameters:", sum(np.product(t.shape) for t in model.parameters()))
+class PieceSquareTable(torch.nn.Module):
+    PIECE_SQUARE_POSITIONS = 2 * 64 * ((6 + 6 + 1) * 64) #13 * 64
+
+    def __init__(self):
+        super().__init__()
+        self.table = torch.nn.EmbeddingBag(
+            num_embeddings=self.PIECE_SQUARE_POSITIONS,
+            embedding_dim=1,
+            mode="sum",
+        )
+        self.tanh = torch.nn.Tanh()
+
+    def adjust_leak(self, new_leak):
+        pass
+
+    def forward(self, indices, offsets, which_model, lengths):
+        value = self.table(indices, offsets)
+        return self.tanh(value)
 
 def process_one_path(path):
     assert path.endswith(".json")
@@ -147,10 +168,15 @@ def train(paths, device="cuda"):
     all_is_duck_move = concat_meta[:, 3]
     print("Constant model loss:", np.var(all_value_for_white))
 
+    # FIXME: This is a hack to emulate not doing the king-relative embeddings.
+    #concat_indices = concat_indices % (13 * 64)
+
     # Begin a training loop.
     model = Nnue().to(device)
+    #model = PieceSquareTable().to(device)
+    print("Parameters:", sum(np.product(t.shape) for t in model.parameters()))
     mse_func = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=2e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0)
     loss_ewma = EWMA()
 
     def make_batch(batch_size):
@@ -170,20 +196,22 @@ def train(paths, device="cuda"):
             torch.tensor(flattened_indices, dtype=torch.int64, device=device),
             torch.tensor(offsets, dtype=torch.int64, device=device),
             torch.tensor(which_model, dtype=torch.int64, device=device),
+            torch.tensor(lengths, dtype=torch.int64, device=device),
             torch.tensor(all_value_for_white[subset].reshape((-1, 1)), dtype=torch.float32, device=device),
         )
 
     start_time = time.time()
     for i in range(1_000_000):
         leak = 0.1 * 0.5 ** (i / 10_000)
-        lr = max(5e-6, 2e-4 * 0.5 ** (i / 20_000))
+        lr = max(5e-6, 5e-4 * 0.5 ** (i / 20_000))
+        #lr = 1e-3
         model.adjust_leak(leak)
         for g in optimizer.param_groups:
             g['lr'] = lr
 
         optimizer.zero_grad()
-        indices, offsets, which_model, value_for_white = make_batch(256)
-        value_output = model(indices, offsets, which_model)
+        indices, offsets, which_model, lengths, value_for_white = make_batch(256)
+        value_output = model(indices, offsets, which_model, lengths)
         loss = mse_func(value_output, value_for_white)
         loss.backward()
         optimizer.step()
