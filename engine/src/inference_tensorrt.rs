879641d68d47cf1253e092e586fd6d8978a08b0f
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2022-11-29 18:50:41 -0800

    Adding TensorRT inference

---
+++ engine/src/inference_tensorrt.rs
@@ -0,0 +1,130 @@
+use std::collections::VecDeque;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+use crate::inference;
+use crate::inference::{
+  InferenceResults, InputBlock, CHANNEL_COUNT, FEATURES_SIZE, POLICY_LEN,
+};
+use crate::tensorrt::TensorRT;
+
+pub struct TensorRTEngine<Cookie> {
+  max_batch_size: usize,
+  tensorrt:       Mutex<TensorRT>,
+  input_blocks:   Mutex<VecDeque<InputBlock<Cookie>>>,
+  pub semaphore:  tokio::sync::Semaphore,
+  eval_count:     std::sync::atomic::AtomicUsize,
+  next_stream_id: std::sync::atomic::AtomicUsize,
+}
+
+impl<Cookie> TensorRTEngine<Cookie> {
+  pub fn new(max_batch_size: usize, model_path: &str) -> Self {
+    let mut tensorrt = TensorRT::new(0, max_batch_size as i32);
+    tensorrt.load_model(model_path);
+    Self {
+      max_batch_size,
+      input_blocks:   Mutex::new(VecDeque::new()),
+      tensorrt:       Mutex::new(tensorrt),
+      semaphore:      tokio::sync::Semaphore::new(0),
+      eval_count:     std::sync::atomic::AtomicUsize::new(0),
+      next_stream_id: std::sync::atomic::AtomicUsize::new(0),
+    }
+  }
+
+  pub fn batch_ready(&self) -> bool {
+    let input_blocks = self.input_blocks.lock().unwrap();
+    input_blocks.front().map(|b| b.cookies.len() == self.max_batch_size).unwrap_or(false)
+  }
+
+  pub fn swap_out_model(&self, model_path: &str) -> Result<(), String> {
+    self.tensorrt.lock().unwrap().load_model(model_path);
+    Ok(())
+  }
+}
+
+impl<Cookie> inference::InferenceEngine<Cookie> for TensorRTEngine<Cookie> {
+  // FIXME: This value doesn't really make sense for the TensorRT engine.
+  const DESIRED_BATCH_SIZE: usize = 128;
+
+  // FIXME: This should just be a part of the InferenceEngine trait.
+  fn add_work(&self, state: &crate::rules::State, cookie: Cookie) -> bool {
+    let mut input_blocks = self.input_blocks.lock().unwrap();
+    let ready =
+      inference::add_to_input_blocks(self.max_batch_size, &mut input_blocks, state, cookie);
+    // FIXME: This notifies too much.
+    if ready {
+      self.semaphore.add_permits(1);
+    }
+    ready
+  }
+
+  fn predict(&self, use_outputs: impl FnOnce(InferenceResults<Cookie>)) -> usize {
+    //let stream_id = self.next_stream_id.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+    //let stream_id = (stream_id % 2) as i32;
+    let stream_id = 0;
+    //let overall_start = std::time::Instant::now();
+    // Pop the last input block, which is inside the mutex.
+    let last_block = {
+      let mut guard = self.input_blocks.lock().unwrap();
+      match guard.pop_front() {
+        Some(block) => block,
+        None => return 0,
+      }
+    };
+    let block_len = last_block.cookies.len();
+    assert!(block_len <= self.max_batch_size);
+    let (inp_features_ptr, out_values_ptr, out_policy_ptr) = {
+      let guard = self.tensorrt.lock().unwrap();
+      guard.get_pointers(stream_id)
+    };
+    // Copy data into the tensorrt input buffer.
+    //let begin_copy = std::time::Instant::now();
+    unsafe {
+      std::ptr::copy_nonoverlapping(
+        last_block.data.as_ptr(),
+        inp_features_ptr,
+        block_len * FEATURES_SIZE,
+      );
+    }
+    //let elapsed = overall_start.elapsed();
+    //let elapsed_copy = begin_copy.elapsed();
+    //println!("Total: {:?} Copy time: {:?}", elapsed, elapsed_copy);
+    // Run the inference.
+    //let start_time = std::time::Instant::now();
+    {
+      let guard = self.tensorrt.lock().unwrap();
+      guard.run_inference(stream_id);
+      guard.wait_for_inference(stream_id);
+    }
+    //tensorrt.run_inference(stream_id);
+    //tensorrt.wait_for_inference(stream_id);
+    self.eval_count.fetch_add(block_len, std::sync::atomic::Ordering::Relaxed);
+    //let elapsed = start_time.elapsed();
+    //println!("Inference time: {:?}", elapsed);
+    //let final_start = std::time::Instant::now();
+    // Copy the output data out of the tensorrt output buffer.
+    let mut policies: Vec<&[f32; POLICY_LEN]> = vec![];
+    for i in 0..block_len {
+      let policy_ptr = unsafe { out_policy_ptr.add(i * POLICY_LEN) };
+      // Transmute this
+      let policy: &[f32; POLICY_LEN] = unsafe { std::mem::transmute(policy_ptr) };
+      //let policy = unsafe { std::slice::from_raw_parts(policy_ptr, POLICY_LEN) };
+      policies.push(policy);
+    }
+    use_outputs(InferenceResults::new(
+      &last_block.cookies,
+      &last_block.players,
+      &policies,
+      unsafe {
+        std::slice::from_raw_parts(out_values_ptr, block_len)
+      },
+    ));
+    //let elapsed = final_start.elapsed();
+    //println!("Use time: {:?}", elapsed);
+    block_len
+  }
+
+  fn clear(&self) {
+    self.input_blocks.lock().unwrap().clear();
+  }
+}
---
+++ engine/src/tensorrt.rs
@@ -0,0 +1,114 @@
+// Rust wrapper for:
+//
+// extern "C" TensorRTWrapper* TensorRTWrapper_new(int cuda_device, int max_batch_size);
+// extern "C" void TensorRTWrapper_delete(TensorRTWrapper* wrapper);
+// extern "C" void TensorRTWrapper_load_model(TensorRTWrapper* wrapper, const char* model_path);
+// extern "C" void TensorRTWrapper_get_pointers(
+//   TensorRTWrapper* wrapper,
+//   int stream_id,
+//   float** inp_features,
+//   float** out_value,
+//   float** out_policy,
+// );
+// extern "C" void TensorRTWrapper_run_inference(TensorRTWrapper* wrapper, int stream_id);
+// extern "C" void TensorRTWrapper_wait_for_inference(TensorRTWrapper* wrapper, int stream_id);
+
+use std::ffi::CString;
+
+extern "C" {
+  pub type TensorRTWrapper;
+  pub fn TensorRTWrapper_new(cuda_device: ::std::os::raw::c_int, max_batch_size: ::std::os::raw::c_int) -> *mut TensorRTWrapper;
+  pub fn TensorRTWrapper_delete(wrapper: *mut TensorRTWrapper);
+  pub fn TensorRTWrapper_load_model(wrapper: *mut TensorRTWrapper, model_path: *const ::std::os::raw::c_char);
+  pub fn TensorRTWrapper_get_pointers(
+    wrapper: *mut TensorRTWrapper,
+    stream_id: ::std::os::raw::c_int,
+    inp_features: *mut *mut ::std::os::raw::c_float,
+    out_value: *mut *mut ::std::os::raw::c_float,
+    out_policy: *mut *mut ::std::os::raw::c_float,
+  );
+  pub fn TensorRTWrapper_run_inference(wrapper: *mut TensorRTWrapper, stream_id: ::std::os::raw::c_int);
+  pub fn TensorRTWrapper_wait_for_inference(wrapper: *mut TensorRTWrapper, stream_id: ::std::os::raw::c_int);
+}
+
+pub struct TensorRT {
+  pointer: *mut TensorRTWrapper,
+}
+
+impl Drop for TensorRT {
+  fn drop(&mut self) {
+    unsafe {
+      TensorRTWrapper_delete(self.pointer);
+    }
+  }
+}
+
+// FIXME: I need to double check that this actually is Send + Sync.
+unsafe impl Send for TensorRT {}
+unsafe impl Sync for TensorRT {}
+
+impl TensorRT {
+  pub fn new(cuda_device: i32, max_batch_size: i32) -> TensorRT {
+    unsafe {
+      TensorRT {
+        pointer: TensorRTWrapper_new(cuda_device, max_batch_size),
+      }
+    }
+  }
+
+  pub fn load_model(&mut self, model_path: &str) {
+    unsafe {
+      let cstr = CString::new(model_path).unwrap();
+      TensorRTWrapper_load_model(self.pointer, cstr.as_ptr());
+    }
+  }
+
+  pub fn get_pointers(&self, stream_id: i32) -> (*mut f32, *mut f32, *mut f32) {
+    let mut inp_features: *mut f32 = std::ptr::null_mut();
+    let mut out_value: *mut f32 = std::ptr::null_mut();
+    let mut out_policy: *mut f32 = std::ptr::null_mut();
+    unsafe {
+      TensorRTWrapper_get_pointers(
+        self.pointer,
+        stream_id,
+        &mut inp_features,
+        &mut out_value,
+        &mut out_policy,
+      );
+    }
+    (inp_features, out_value, out_policy)
+  }
+
+  pub fn run_inference(&self, stream_id: i32) {
+    unsafe {
+      TensorRTWrapper_run_inference(self.pointer, stream_id);
+    }
+  }
+
+  pub fn wait_for_inference(&self, stream_id: i32) {
+    unsafe {
+      TensorRTWrapper_wait_for_inference(self.pointer, stream_id);
+    }
+  }
+}
+
+pub fn use_tensorrt() {
+  let mut tensorrt = TensorRT::new(0, 128);
+  tensorrt.load_model("/tmp/trt_out");
+  let (inp_features, out_value, out_policy) = tensorrt.get_pointers(0);
+  unsafe {
+    *inp_features = 1.0;
+  }
+  // See how fast we can run inference.
+  let start = std::time::Instant::now();
+  for _ in 0..1000 {
+    tensorrt.run_inference(0);
+    tensorrt.wait_for_inference(0);
+    //tensorrt.run_inference(1);
+    //tensorrt.wait_for_inference(2);
+    //tensorrt.run_inference(2);
+    //tensorrt.wait_for_inference(0);
+  }
+  let elapsed = start.elapsed();
+  println!("Elapsed: {:?}", elapsed);
+}
