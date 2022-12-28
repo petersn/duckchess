5c9244ff57005fe9d40259ce1a60b7eff372e476
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2022-12-27 20:06:54 -0800

    Updating for run 14

--- engine/looper.sh
+++ engine/looper.sh
@@ -4,4 +4,4 @@ set -x
 RUN_DIR=$(cat CURRENT_RUN)
 
 #time python ml/looper.py --parallel-games-processes 5 --training-window 50 --game-count 5000 --prefix $RUN_DIR
-time python ml/looper.py --parallel-games-processes 5 --training-window 50 --game-count 4000 --prefix $RUN_DIR
+time python ml/looper.py --parallel-games-processes 5 --training-window 50 --game-count 4500 --prefix $RUN_DIR
