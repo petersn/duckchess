482011c7b76461c4140bf8d7859f4dbaa6da0171
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2023-01-13 20:36:15 -0800

    Remove old stuff

--- src/EngineWorker.ts
+++ src/EngineWorker.ts
@@ -1,4 +1,4 @@
-import init, { new_engine, max_batch_size, channel_count, parse_pgn4, Engine, perft, perft_nnue, perft_eval, test_threads, test_simd, test_shared_mem, new_game_tree } from 'engine';
+import init, { new_engine, max_batch_size, channel_count, parse_pgn4, Engine, perft, perft_nnue, perft_eval, new_game_tree } from 'engine';
 import * as tf from '@tensorflow/tfjs';
 import { MessageFromEngineWorker, MessageToEngineWorker, ModelName } from './DuckChessEngine';
 import { threads } from 'wasm-feature-detect';
