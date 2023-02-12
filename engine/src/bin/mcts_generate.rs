ccd55271abd8bb18a829650de20d43c97be26c3c
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2023-02-12 02:02:40 -0800

    Increase visits in self-play

--- engine/src/bin/mcts_generate.rs
+++ engine/src/bin/mcts_generate.rs
@@ -9,8 +9,8 @@ use tokio::io::AsyncBufReadExt;
 use tokio::sync::Mutex;
 
 const PLAYOUT_CAP_RANDOMIZATION_P: f32 = 0.25;
-const FULL_SEARCH_PLAYOUTS: u32 = 1000;
-const SMALL_SEARCH_PLAYOUTS: u32 = 200;
+const FULL_SEARCH_PLAYOUTS: u32 = 1500;
+const SMALL_SEARCH_PLAYOUTS: u32 = 250;
 //const FULL_SEARCH_PLAYOUTS: u32 = 600;
 //const SMALL_SEARCH_PLAYOUTS: u32 = 100;
 const GAME_LEN_LIMIT: usize = 800;
